94e03f9172519ae1ce5bde9677581bb97c6b94ac
asma <asmafal@gmail.com>
2022-01-22 11:34:38 -0600

    q11 done

--- AvengerPracticeHour/Day_06_01_22_2022/question11/BasePage.java
+++ AvengerPracticeHour/Day_06_01_22_2022/question11/BasePage.java
@@ -1,7 +1,8 @@
 package Day_06_01_22_2022.question11;
 
 public class BasePage {
-    public BasePage(){
+
+    public BasePage(){//not default constructor but a non parameterized constructor
         System.out.println("I am in Base Page");
     }
 }

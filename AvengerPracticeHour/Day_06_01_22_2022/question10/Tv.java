af5fdca80ba510450946d145e309976a21d4ca7d
asma <asmafal@gmail.com>
2022-01-22 11:20:07 -0600

    unit 4 review hour 3

--- AvengerPracticeHour/Day_06_01_22_2022/question10/Tv.java
+++ AvengerPracticeHour/Day_06_01_22_2022/question10/Tv.java
@@ -1,11 +1,12 @@
 package Day_06_01_22_2022.question10;
 
 public class Tv {
+
     private void init() {
         System.out.println("Initialized");
     }
 
-    private void start () {
+    public void start () {
         init(); // line 2
         System.out.println("Started");
     }

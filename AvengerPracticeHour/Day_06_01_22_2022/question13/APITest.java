feb07dc0112ab4d12164d1da4e974b00dda9b21a
asma <asmafal@gmail.com>
2022-01-22 11:49:30 -0600

    q13 done

--- AvengerPracticeHour/Day_06_01_22_2022/question13/APITest.java
+++ AvengerPracticeHour/Day_06_01_22_2022/question13/APITest.java
@@ -2,12 +2,9 @@ package Day_06_01_22_2022.question13;
 
 public class APITest extends FunctionalTest{
 
-    // implements code here
-
-
     @Override
     protected void print() {
-        super.print();
+        //super.print();
         System.out.println("I am doing API testing");
     }
 }

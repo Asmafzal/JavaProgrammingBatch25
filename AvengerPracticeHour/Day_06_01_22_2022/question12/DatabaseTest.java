8aed2a2297606d428acfa7dac3b6351bc26b66f8
asma <asmafal@gmail.com>
2022-01-22 11:42:35 -0600

    q12 done

--- AvengerPracticeHour/Day_06_01_22_2022/question12/DatabaseTest.java
+++ AvengerPracticeHour/Day_06_01_22_2022/question12/DatabaseTest.java
@@ -1,6 +1,7 @@
 package Day_06_01_22_2022.question12;
 
 public class DatabaseTest extends Database{
+
     public static void main(String[] args) {
         Database obj = new Database();
         obj.setName ("Cybertek"); // line 3

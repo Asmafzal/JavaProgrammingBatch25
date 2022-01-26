38a8eabdb0056c8de5368b139421e4b736b8bd08
asma <asmafal@gmail.com>
2022-01-26 15:59:32 -0600

    inheritance office hours

---
+++ officeHour/practice_01_26_2022/inhertanceAppTask/AppObject.java
@@ -0,0 +1,12 @@
+package practice_01_26_2022.inhertanceAppTask;
+
+public class AppObject {
+    public static void main(String[] args) {
+
+        Instagram instagram = new Instagram("5.6");
+        System.out.println("Instagram "+instagram);
+
+        instagram.useTheApp(10);
+
+    }
+}
---
+++ officeHour/practice_01_26_2022/inhertanceAppTask/MobileApp.java
@@ -0,0 +1,63 @@
+package practice_01_26_2022.inhertanceAppTask;
+
+public class MobileApp {
+    /*
+    Create a class called MobileApp
+
+            instance variables:
+                name, version
+
+                add a constructor to set all the fields
+
+            instance methods:
+                useTheApp(int minutes), download()
+
+        Create the following sub classes of MobileApp:
+            1. Instagram:
+                    extra methods:
+                        postPhoto()
+            2. Discord:
+                    extra methods:
+                        chat(String name)
+     */
+
+    private String name, version;
+
+    public MobileApp(String name, String version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public void useTheApp(int minutes){
+        System.out.println("Using "+name+" app for "+minutes+" minutes");
+    }
+
+    public void downloadMethod(){
+        System.out.println("App "+name +" version "+ version +" is downloading");
+
+    }
+
+    @Override
+    public String toString() {
+        return "MobileApp{" +
+                "name='" + name + '\'' +
+                ", version='" + version + '\'' +
+                '}';
+    }
+}

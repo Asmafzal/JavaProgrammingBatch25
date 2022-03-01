ec9b737fd06240191e792cafcabdd3b7811d5e86
asma <asmafal@gmail.com>
2022-03-01 09:56:11 -0600

    day 51 map class

--- src/day51_Map/MapClasses.java
+++ src/day51_Map/MapClasses.java
@@ -14,20 +14,41 @@ public class MapClasses {
         hashMap.put(5, "Isabella");
         hashMap.put(5, "Ahmet"); //the last one will be kept...the one before will be deleted...key has to be unique
         //hashMap is fastest of the maps...the performance, less memory, thats why its used more ..but the order is not guaranteed
+        //supports null
         
         System.out.println("hashMap = " + hashMap);
+
+
         Map<Integer, String> linkedHashMap = new LinkedHashMap<>();
         linkedHashMap.put(10, "Arthur");
         linkedHashMap.put(20, "George");
         linkedHashMap.put(3, "Jack");
         linkedHashMap.put(40, "Emma");
         linkedHashMap.put(5, "Isabella");
-        //the insertion order of the linkedhashmap is preserved
+        //the insertion order of the linkedhashmap is preserved...supports null
 
-        Map<Integer, String> map3 = new TreeMap<>();
+        System.out.println("linkedHashMap = " + linkedHashMap);
 
-        Map<Integer, String> map4 = new Hashtable<>();
+        Map<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(10, "Arthur");
+        treeMap.put(20, "George");
+        treeMap.put(3, "Jack");
+        treeMap.put(40, "Emma");
+        treeMap.put(5, "Isabella");
+        //does not support null
 
+        System.out.println("treeMap = " + treeMap);
+
+
+        Map<Integer, String> hashtable = new Hashtable<>();
+        hashtable.put(10, "Arthur");
+        hashtable.put(20, "George");
+        hashtable.put(3, "Jack");
+        hashtable.put(40, "Emma");
+        hashtable.put(5, "Isabella");
+        hashtable.put(5, "Ahmet");
+
+        System.out.println("hashtable = " + hashtable);
 
 
     }
